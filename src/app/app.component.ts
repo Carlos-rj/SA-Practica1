21523208eac1dbbd0f8b132749d20317037ad482
Carlos_rj <carlosjimenez2286@gmail.com>
2020-12-16 20:39:08 -0600

    Practica2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre: string = "Carlos Roberto";
+  nombre: string = "Carlos Roberto Jimenez Perez";
   carnet: number = 201504143;
 
 }
